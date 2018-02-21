e792c5f1f1542845a868de019f7da89e94f8ffeb
ifodde <iostofodde@gmail.com>
2018-02-21 14:54:48 +0100

    atmosphere (nrlmsise) dependent knudsen number

--- Tudat/Astrodynamics/Aerodynamics/flightConditions.h
+++ Tudat/Astrodynamics/Aerodynamics/flightConditions.h
@@ -19,9 +19,13 @@
 #include "Tudat/Astrodynamics/Aerodynamics/trimOrientation.h"
 #include "Tudat/Astrodynamics/Aerodynamics/aerodynamicCoefficientInterface.h"
 #include "Tudat/Astrodynamics/Aerodynamics/atmosphereModel.h"
+#include "Tudat/Astrodynamics/Aerodynamics/nrlmsise00Atmosphere.h"
+#include "Tudat/Astrodynamics/Aerodynamics/nrlmsise00InputFunctions.h"
 #include "Tudat/Astrodynamics/BasicAstrodynamics/bodyShapeModel.h"
 #include "Tudat/Astrodynamics/ReferenceFrames/aerodynamicAngleCalculator.h"
 #include "Tudat/Basics/basicTypedefs.h"
+#include "Tudat/Mathematics/BasicMathematics/mathematicalConstants.h"
+
 
 namespace tudat
 {
@@ -226,14 +230,28 @@ public:
 
     double getCurrentKnudsenNumber( )
     {
-        double mach = getCurrentMachNumber();
-        double velocity = getCurrentAirspeed();
-        double refLength = 5.0;
-        double density = getCurrentDensity();
-        double viscosity = 0.000035;
-        double specHeatRatio = 1.3;
-        double Reynolds = density*velocity*refLength/viscosity;
-        return mach/Reynolds * sqrt(specHeatRatio*3.14/2);
+        const double AVOGADRO_CONSTANT = 6.02214129E23;
+        double characteristicLength = 5.0;
+        boost::shared_ptr< aerodynamics::NRLMSISE00Atmosphere > earthAtmosphereModel =
+                boost::dynamic_pointer_cast< aerodynamics::NRLMSISE00Atmosphere >( atmosphereModel_ );
+        double molMass = earthAtmosphereModel->getMeanMolarMass(scalarFlightConditions_.at( altitude_flight_condition ),
+                                                            scalarFlightConditions_.at( longitude_flight_condition ),
+                                                            scalarFlightConditions_.at( latitude_flight_condition ), currentTime_);
+        double colDiameter = earthAtmosphereModel->getWeightedAverageCollisionDiameter(scalarFlightConditions_.at( altitude_flight_condition ),
+                                                                                   scalarFlightConditions_.at( longitude_flight_condition ),
+                                                                                   scalarFlightConditions_.at( latitude_flight_condition ), currentTime_);
+        double Density = getCurrentDensity();
+        double particleMass = (molMass / 1000.0) / AVOGADRO_CONSTANT;
+        return particleMass / (std::sqrt(2.0) *mathematical_constants::PI *
+                            std::pow(colDiameter,2.0) * Density) / characteristicLength;
+        //double mach = getCurrentMachNumber();
+        //double velocity = getCurrentAirspeed();
+        //double refLength = 5.0;
+        //double density = getCurrentDensity();
+        //double viscosity = 0.000035;
+        //double specHeatRatio = 1.3;
+        //double Reynolds = density*velocity*refLength/viscosity;
+        //return mach/Reynolds * sqrt(specHeatRatio*3.14/2);
     }
 
     //! Function to return atmosphere model object
